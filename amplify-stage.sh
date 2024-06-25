a96ace2c79c40774a4313c6a67783be096706865
vkantchev <vkantchev@users.noreply.github.com>
2024-06-25 13:32:40 -0700

    Use in-memory database for demo Amplify deployments

--- amplify-stage.sh
+++ amplify-stage.sh
@@ -3,9 +3,9 @@
 amplify_dir=./amplify/backend/api/swiftapirest/src
 
 # App 
-cp -r src $amplify_dir/
-cp requirements.txt $amplify_dir/
+cp -rp src $amplify_dir/
+cp -p requirements.txt $amplify_dir/
 
 # Docker
-cp docker/Dockerfile $amplify_dir/
-cp docker/docker-compose.yml $amplify_dir/
+cp -p docker/Dockerfile $amplify_dir/
+cp -p docker/docker-compose.yml $amplify_dir/
